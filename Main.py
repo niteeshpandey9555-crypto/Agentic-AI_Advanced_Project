065c20bd06993c6480d8218f4db99e19dc489730
Niteesh Pandey <niteeshpandey9555@gmail.com>
2026-01-06 04:41:49 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,11 @@
+from tools.task_tools import (
+    send_notification,
+    complete_task,
+    check_task_status,
+    give_feedback
+)
+
+print(send_notification("AI Planning Task"))
+print(complete_task("AI Planning Task"))
+print(check_task_status("AI Planning Task"))
+print("Reward:", give_feedback(success=True))
